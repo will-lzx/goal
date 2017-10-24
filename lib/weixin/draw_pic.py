01a9d2216b2f9b9c1c8405e42310c7859d7694b6
will-lzx <will-liu@live.com>
2017-10-24 09:31:37 +0800

    fix the report

--- lib/weixin/draw_pic.py
+++ lib/weixin/draw_pic.py
@@ -12,28 +12,28 @@ def draw(low_img, headimg, author_name, goal_content, penalty, tow_dimension, sa
 
     im2 = Image.open(headimg)
 
-    #im3 = Image.open(tow_dimension)
+    im3 = Image.open(tow_dimension)
 
     # 在图片上添加文字 1
     draw_handle = ImageDraw.Draw(im1)
     draw_handle.bitmap((200, 600), im2, (255, 255, 0))
 
-    # draw_handle.text((0, 0), author_name + "的小目标", (255, 255, 0))
-    # draw_handle.text((0, 0), goal_content, (255, 255, 0))
-    #
-    # draw_handle.text((0, 0), '如完不成则自罚', (255, 255, 0))
-    #
-    # draw_handle.text((0, 0), penalty, (255, 255, 0))
-    #
-    # draw_handle.bitmap((0, 0), im3, (255, 255, 0))
-    #
-    # show_content1 = '长按识别二维码吗围观监督TA的小目标'
-    #
-    # draw_handle.text((0, 0), show_content1, (255, 255, 0))
-    #
-    # show_content2 = '世界会向那些有目标和远见的人让路'
-    #
-    # draw_handle.text((0, 0), show_content2, (255, 255, 0))
+    draw_handle.text((600, 800), author_name + "的小目标", (255, 255, 0))
+    draw_handle.text((700, 800), goal_content, (255, 255, 0))
+
+    draw_handle.text((800, 800), '如完不成则自罚', (255, 255, 0))
+
+    draw_handle.text((900, 800), penalty, (255, 255, 0))
+
+    draw_handle.bitmap((1000, 800), im3, (255, 255, 0))
+
+    show_content1 = '长按识别二维码吗围观监督TA的小目标'
+
+    draw_handle.text((1200, 800), show_content1, (255, 255, 0))
+
+    show_content2 = '世界会向那些有目标和远见的人让路'
+
+    draw_handle.text((1400, 800), show_content2, (255, 255, 0))
 
     draw_handle = ImageDraw.Draw(im1)
 
