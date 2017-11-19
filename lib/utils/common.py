71feddd1c798aec8eed2c84bbd499928b82f5659
will-lzx <will-liu@live.com>
2017-11-19 15:31:30 +0800

    get user info

--- lib/utils/common.py
+++ lib/utils/common.py
@@ -25,6 +25,15 @@ def get_access_token():
     return access_token
 
 
+def get_openid(code):
+    url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid={0}&secret={1}&code={2}&grant_type=authorization_code'.format(
+        WEIXIN_APPID, WEIXIN_APPSECRET, code)
+
+    url_req = UrlRequest()
+    resp = url_req.url_request(url)
+    return resp['openid']
+
+
 def get_user_info(openid):
     client = WeChatClient(WEIXIN_APPID, WEIXIN_APPSECRET)
     user = client.user.get(openid)
