0c3e882877b0d14b28637786ae69b23a00dcd638
will-lzx <will-liu@live.com>
2017-11-20 11:14:07 +0800

    get user info

--- lib/weixin/draw_pic.py
+++ lib/weixin/draw_pic.py
@@ -1,29 +1,30 @@
 #*- coding: utf-8*-
 import io
+import urllib
+from io import StringIO
 
 import PIL
+import time
 from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
 from urllib.request import urlopen
+import os
 
 
 def draw(low_img, headimg, author_name, goal_create_time, goal_content, penalty, two_dimension, save_img):
 
     font = ImageFont.truetype('/var/www/goal/static/images/SourceHanSansCN-Bold.otf', 24)
+    #font = ImageFont.truetype('/Users/zhixiangliu/Documents/code/goal/static/images/SourceHanSansCN-Bold.otf', 24)
     im1 = Image.open(low_img)
 
     width, height = im1.size
 
-    image_bytes = urlopen('http://wx.qlogo.cn/mmopen/ajNVdqHZLLCCzGDKibYicjyeVcylKsWQANxnlNxcyZQPFF3ItyUD6iawVcEBXHiadenx57wkqmouqyzIopl4gZVydQ/0').read()
+    tmp_img = os.path.join('/var/www/goal/static/tmp/', str(time.time()) + '.jpg')
 
-    data_stream = io.BytesIO(image_bytes)
+    im2 = convert_img(headimg, tmp_img)
 
-    im2 = Image.open(data_stream)
-
-    im2 = im2.resize((80, 80), PIL.Image.ANTIALIAS)
-
-    im2_width, im2_height = im2.size
+    remove_img_file(tmp_img)
 
     #new_png = transparent('/Users/zhixiangliu/Documents/code/goal/static/images/getqrcode.png', '/Users/zhixiangliu/Documents/code/goal/static/images/getqrcode2.png')
 
@@ -32,13 +33,14 @@ def draw(low_img, headimg, author_name, goal_create_time, goal_content, penalty,
 
     # 在图片上添加文字 1
     draw_handle = ImageDraw.Draw(im1)
-    draw_handle.bitmap((20, 20), im2, (0, 0, 0))
+    draw_handle.bitmap((20, 20), im2, (255, 255, 255))
 
     draw_handle.text((130, 38), author_name + '定下小目标', (0, 0, 0), font)
 
     draw_handle.text((130, 75),  str(goal_create_time), (0, 0, 0), font)
 
     content_font = ImageFont.truetype('/var/www/goal/static/images/SourceHanSansCN-Bold.otf', 80)
+    #content_font = ImageFont.truetype('/Users/zhixiangliu/Documents/code/goal/static/images/SourceHanSansCN-Bold.otf', 80)
 
     w, h = content_font.getsize(goal_content)
     draw_handle.text((width/2 - w/2, 200), goal_content, (0, 0, 0), content_font)
@@ -74,9 +76,34 @@ def create_two_dimension(save_img):
     return save_img
 
 
+def convert_img(headimg, save_img):
+    image_bytes = urlopen(headimg).read()
+
+    data_stream = io.BytesIO(image_bytes)
+    print(data_stream)
+
+    im = Image.open(data_stream)
+    im.save(save_img)
+
+    card = Image.new("RGBA", (800, 600), (255, 255, 255))
+    img = Image.open(save_img).convert("RGBA")
+    x, y = img.size
+    card.paste(img, (0, 0, x, y))
+    card = img.resize((80, 80), PIL.Image.ANTIALIAS)
+
+    return card
+
+
+def remove_img_file(src):
+    try:
+        os.remove(src)
+    except Exception as ex:
+        print('remove file failed, ', ex)
+
+
 if __name__ == '__main__':
-    low_img = '/Users/zhixiangliu/Documents/code/goal/static/images/kanshu.jpg'
-    headimg = '/Users/zhixiangliu/Documents/code/goal/static/images/shengji.png'
+    low_img = '/Users/zhixiangliu/Documents/code/goal/static/images/dushu.jpg'
+    headimg = 'http://wx.qlogo.cn/mmopen/ajNVdqHZLLCCzGDKibYicjyeVcylKsWQANxnlNxcyZQPFF3ItyUD6iawVcEBXHiadenx57wkqmouqyzIopl4gZVydQ/0'
     author_name = '刘志祥'
     goal_content = '我的微目标'
     penalty = '裸奔'
