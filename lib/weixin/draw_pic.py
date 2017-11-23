f7c1942c501fac43802e69c997c0323abfde6eab
will-lzx <will-liu@live.com>
2017-11-23 21:16:19 +0800

    fix the audience page

--- lib/weixin/draw_pic.py
+++ lib/weixin/draw_pic.py
@@ -46,7 +46,7 @@ def draw(low_img, headimg, author_name, goal_create_time, goal_content, penalty,
     w, h = content_font.getsize(goal_content)
 
     if len(goal_content) > 6:
-        goal_content1 = goal_content[0, len(goal_content)/2]
+        goal_content1 = goal_content[0: len(goal_content)/2]
         goal_content2 = goal_content[len(goal_content) / 2:]
         w1, h1 = content_font.getsize(goal_content1)
         w2, h2 = content_font.getsize(goal_content2)
@@ -58,7 +58,7 @@ def draw(low_img, headimg, author_name, goal_create_time, goal_content, penalty,
         draw_handle.text((width/2 - w/2, 200), goal_content, (0, 0, 0), content_font)
 
     w, h = content_font.getsize(penalty)
-    
+
     draw_handle.text((width/2 - w/2, 600), penalty, (0, 0, 0), content_font)
 
     draw_handle.bitmap((52, height-144), im3, (255, 255, 255))
