54061d0ff6706fdead4748fa89474b407c6e1aab
will-lzx <will-liu@live.com>
2017-11-25 17:43:40 +0800

    fix the upload pics

--- lib/weixin/weixin_sql.py
+++ lib/weixin/weixin_sql.py
@@ -131,9 +131,12 @@ def save_history_image(history_id, image_url, index):
 
     #data_stream = io.BytesIO(image_bytes)
 
-    fin = open(image_url, 'rb')
-    data = fin.read()
-    fin.close()
+    try:
+        fin = open(image_url, 'rb')
+        data = fin.read()
+        fin.close()
+    except Exception as ex:
+        print(ex)
     #data = data_stream
     #print('data', data)
 
